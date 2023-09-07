bae5eb96e6ac8611c5c3bd8bdcbd155d78bafb1f
Matti Tahvonen <matti@vaadin.com>
2023-09-07 09:46:08 +0300

    improved tinymce demo

--- src/main/java/org/example/views/TinyMceView.java
+++ src/main/java/org/example/views/TinyMceView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H5;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.example.Addon;
@@ -12,6 +13,7 @@ import org.example.DefaultLayout;
 import org.jsoup.safety.Safelist;
 import org.vaadin.firitin.appframework.MenuItem;
 import org.vaadin.firitin.components.RichText;
+import org.vaadin.firitin.components.orderedlayout.VHorizontalLayout;
 import org.vaadin.tinymce.TinyMce;
 
 @Route(layout = DefaultLayout.class)
@@ -20,16 +22,31 @@ import org.vaadin.tinymce.TinyMce;
 public class TinyMceView extends VerticalLayout {
 
     public TinyMceView() {
+        add(new RichText().withMarkDown("""
+        # TinyMCE editor
+        
+        TinyMCE is a platform independent web based Javascript HTML WYSIWYG editor control released as Open Source under LGPL.
+        
+        See more configuration options from [add-on tests](https://github.com/parttio/tinymce-for-flow/tree/master/src/test/java/org/vaadin/tinymce) or [TinyMCE documentation](https://www.tiny.cloud/docs/configure/).
+        """));
+
         TinyMce editor = new TinyMce();
         String originalValue = "This is a <b>bold</b> text with <i>italic</i> text.";
         editor.setValue(originalValue);
+        editor.setWidth("100%");
 
         Div rawHtml = new Div();
+        rawHtml.setWidth("100%");
 
         Button showValue = new Button("show value", event -> {
             rawHtml.getElement().setProperty("innerHTML", editor.getValue());
         });
 
-        add(editor, showValue, new H5("Value:"), rawHtml);
+        add(new VHorizontalLayout(editor, new VerticalLayout(
+                        showValue,
+                        new H5("Value:"),
+                        rawHtml
+                )
+        ).withFullWidth());
     }
 }
