c3baf701f34d6d9d20852fa7e84ba2e82d5387d9
Matti Tahvonen <matti@vaadin.com>
2025-04-04 12:51:17 +0300

    Additional refactoring

--- src/main/java/org/example/views/viritin/PagingGridView.java
+++ src/main/java/org/example/views/viritin/PagingGridView.java
@@ -23,7 +23,7 @@ import org.vaadin.firitin.layouts.HorizontalFloatLayout;
 @Addon("flow-viritin")
 public class PagingGridView extends VerticalLayout {
 
-    private final PagingGrid<Person> table = new PagingGrid<>(Person.class);
+    private final PersonPagingGrid table = new PersonPagingGrid();
     private final DataService dao;
 
     public PagingGridView(DataService service) {
@@ -38,7 +38,72 @@ public class PagingGridView extends VerticalLayout {
         
         """));
 
-        add(new TextField() {{
+        add(new FirstNameFilter(), table);
+
+        table.list(""); // Initial load with no filter
+
+
+        // options/democonfigurations
+        add(new HorizontalFloatLayout(){{
+            // Optional
+            // If you know, or some further long running task can detect the size of
+            // results, it can be defined later:
+            Button b = new Button("Define size to 1000", e -> {
+                table.setTotalResults(1000);
+            });
+            // Optional
+            // If you know, or some further long running task can detect the size of
+            // results, it can be defined later:
+            Button b2 = new Button("Define size to 90", e -> {
+                table.setTotalResults(90);
+            });
+
+            Select<PagingGrid.PaginationBarMode> select = new Select<>();
+            select.setLabel("Pagination bar mode");
+            select.setItems(PagingGrid.PaginationBarMode.values());
+            select.setValue(PagingGrid.PaginationBarMode.TOP);
+            select.addValueChangeListener(e -> {
+                table.setPaginationBarMode(e.getValue());
+            });
+
+            add(new FirstNameFilter(), table, new HorizontalFloatLayout(b, b2, select));
+        }});
+
+
+    }
+
+
+    private class PersonPagingGrid extends PagingGrid<Person> {
+
+        public PersonPagingGrid() {
+            super(Person.class);
+            setColumns("id", "firstName", "lastName", "age");
+        }
+
+        public void list(String filter) {
+            table.setPagingDataProvider((page, pageSize) -> {
+                // This is demo specific line, normally, e.g. with spring data, page number is
+                // enough
+                int start = (int) (page * table.getPageSize());
+                String sortProperty;
+                boolean asc = true;
+                // Optional, sorting
+                if (!table.getSortOrder().isEmpty()) {
+                    GridSortOrder<Person> sortOrder = table.getSortOrder().get(0);
+                    sortProperty = sortOrder.getSorted().getKey();
+                    asc = sortOrder.getDirection() == SortDirection.ASCENDING;
+                } else {
+                    // sort by id for stability if nothing else defined
+                    sortProperty = "id";
+                }
+                return dao.findPersons(filter, start, pageSize, "id", true);
+            });
+        }
+
+    }
+
+    private class FirstNameFilter extends TextField {
+        {
             setValueChangeMode(ValueChangeMode.LAZY);
             setClearButtonVisible(true);
             setPlaceholder("Filter by first name");
@@ -47,59 +112,9 @@ public class PagingGridView extends VerticalLayout {
                 String filter = e.getValue();
                 // Define results with a simpler data provider API, that just gives you page to
                 // request
-                list(filter);
+                table.list(filter);
             });
 
-        }});
-
-        table.setColumns("id", "firstName", "lastName", "age");
-
-
-        list(""); // Initial load with no filter
-
-        // Optional
-        // If you know, or some further long running task can detect the size of
-        // results, it can be defined later:
-        Button b = new Button("Define size to 1000", e -> {
-            table.setTotalResults(1000);
-        });
-        // Optional
-        // If you know, or some further long running task can detect the size of
-        // results, it can be defined later:
-        Button b2 = new Button("Define size to 90", e -> {
-            table.setTotalResults(90);
-        });
-
-        Select<PagingGrid.PaginationBarMode> select = new Select<>();
-        select.setLabel("Pagination bar mode");
-        select.setItems(PagingGrid.PaginationBarMode.values());
-        select.setValue(PagingGrid.PaginationBarMode.TOP);
-        select.addValueChangeListener(e -> {
-            table.setPaginationBarMode(e.getValue());
-        });
-
-        add(table, new HorizontalFloatLayout(b, b2, select));
-
+        }
     }
-
-    private void list(String filter) {
-        table.setPagingDataProvider((page, pageSize) -> {
-            // This is demo specific line, normally, e.g. with spring data, page number is
-            // enough
-            int start = (int) (page * table.getPageSize());
-            String sortProperty;
-            boolean asc = true;
-            // Optional, sorting
-            if (!table.getSortOrder().isEmpty()) {
-                GridSortOrder<Person> sortOrder = table.getSortOrder().get(0);
-                sortProperty = sortOrder.getSorted().getKey();
-                asc = sortOrder.getDirection() == SortDirection.ASCENDING;
-            } else {
-                // sort by id for stability if nothing else defined
-                sortProperty = "id";
-            }
-            return dao.findPersons(filter, start, pageSize, "id", true);
-        });
-    }
-
 }
